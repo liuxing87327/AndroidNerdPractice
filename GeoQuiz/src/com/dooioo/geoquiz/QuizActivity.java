6815b67af2fb69d9c1daef9d0bc161e434b28026
刘兴 <liuxing87327@163.com>
2014-11-19 00:54:52 +0800

    可以旋转屏幕展示不同的布局
    1.展示不同布局
    2.状态保存
    3.activity生命周期

--- GeoQuiz/src/com/dooioo/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/dooioo/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.dooioo.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,6 +10,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -40,6 +44,10 @@ public class QuizActivity extends Activity {
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX);
+        }
 
         updateQuestion();
 
@@ -116,6 +124,45 @@ public class QuizActivity extends Activity {
                 showMessage(R.string.next_button);
             }
         });
+
+        Log.d(TAG, "onCreate called");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle onSaveInstanceState) {
+        super.onSaveInstanceState(onSaveInstanceState);
+        Log.d(TAG, "onSaveInstanceState");
+        onSaveInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
     }
 
     /**
